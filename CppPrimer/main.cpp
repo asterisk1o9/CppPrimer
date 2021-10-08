f111670f6918e9a21efd4c39563ff6e95de154f2
Marvin Augustin <maugu028@fiu.edu>
2021-10-08 13:12:21 -0400

    Initial Commit

---
+++ CppPrimer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CppPrimer
+//
+//  Created by Marvin Augustin on 10/8/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
